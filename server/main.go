f1a6fab14410fc6aede8f5e811103b63279cd7c2
yona3 <kou.y.0905@icloud.com>
2021-09-26 12:50:46 +0900

    add: godotenv

--- server/main.go
+++ server/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	// load .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	id := os.Getenv("GOOGLE_CLIENT_ID")
+	fmt.Println(id)
 }
