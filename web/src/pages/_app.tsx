119e04d53b94295c7c549c67d426248366359747
yona3 <kou.y.0905@icloud.com>
2021-09-29 18:43:43 +0900

    add: Recoil

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import "tailwindcss/tailwind.css";
 
 import type { AppProps } from "next/dist/shared/lib/router/router";
+import { RecoilRoot } from "recoil";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 };
 
 export default App;
