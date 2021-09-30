8d12b79d4dfc144412adc6e57d30a2a64a6f07c4
yona3 <kou.y.0905@icloud.com>
2021-09-30 17:26:21 +0900

    add: About page Head

--- web/src/pages/about.tsx
+++ web/src/pages/about.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import Link from "next/link";
 import { Layout } from "src/components/Layout";
 
 const About: NextPage = () => {
   return (
     <Layout>
+      <Head>
+        <title>About</title>
+        <meta name="description" content="This is the about page." />
+      </Head>
+
       <div className="mt-12 text-center">
         <h1 className="text-lg">About</h1>
         <p className="mt-4">This is the about page.</p>
